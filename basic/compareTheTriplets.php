89eb9439b84ee81b52ec5aac78f7845a102c73fe
AssoDePicche <samuelprado730@gmail.com>
2022-10-06 23:11:56 -0300

    Update Compare the Triplets

--- basic/compareTheTriplets.php
+++ basic/compareTheTriplets.php
@@ -2,17 +2,18 @@
 
 function compareTheTriplets(array $a, array $b): array
 {
-    $score = [0, 0];
+    $alice = 0;
+    $bob = 0;
 
     for ($i = 0; $i < 3; $i++) {
         if ($a[$i] > $b[$i]) {
-            $score[0] += 1;
+            $alice += 1;
         }
 
         if ($a[$i] < $b[$i]) {
-            $score[1] += 1;
+            $bob += 1;
         }
     }
 
-    return $score;
+    return [$alice,$bob];
 }
