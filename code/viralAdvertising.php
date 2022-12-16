1a54e242a25458271d5340ff3b5ce7e0ca26e880
AssoDePicche <samuelprado730@gmail.com>
2022-12-16 18:11:06 -0300

    Feat: Add Viral Advertising

---
+++ code/viralAdvertising.php
@@ -0,0 +1,16 @@
+<?php
+
+function viralAdvertising(int $day): int
+{
+    $likes = 0;
+    $cumulative = 0;
+    $recipients = 5;
+
+    for ($currentDay = 1; $currentDay <= $day; $currentDay++) {
+        $likes = floor($recipients / 2);
+        $recipients = $likes * 3;
+        $cumulative += $likes;
+    }
+
+    return $cumulative;
+}
