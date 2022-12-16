2c836fadefbfa0c7ff5297b8f52d5cb48f7bf91e
AssoDePicche <samuelprado730@gmail.com>
2022-12-16 14:15:16 -0300

    Refactor: Update the if conditionals to a ternary operation

--- code/compareTheTriplets.php
+++ code/compareTheTriplets.php
@@ -1,18 +1,13 @@
 <?php
 
-function compareTheTriplets(array $a, array $b): array
+function compareTriplets(array $a, array $b): array
 {
     $alice = 0;
     $bob = 0;
 
     for ($i = 0; $i < 3; $i++) {
-        if ($a[$i] > $b[$i]) {
-            $alice += 1;
-        }
-
-        if ($a[$i] < $b[$i]) {
-            $bob += 1;
-        }
+        $a[$i] > $b[$i] && $alice++;
+        $a[$i] < $b[$i] && $bob++;
     }
 
     return [$alice,$bob];
