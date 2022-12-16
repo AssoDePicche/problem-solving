143022dde4eefeeace43e482aa7f0a3449827f02
AssoDePicche <samuelprado730@gmail.com>
2022-12-16 12:28:38 -0300

    Add Picking Numbers

---
+++ code/pickingNumbers.php
@@ -0,0 +1,28 @@
+<?php
+
+function frequency(array $search, int $n): array
+{
+    $range = range(0, $n);
+    $elementsInRange = count($range);
+    $searchLength = count($search);
+    $frequency = array_fill(0, $elementsInRange, 0);
+
+    for ($index = 0; $index < $searchLength; $index++) {
+        $frequency[$search[$index]]++;
+    }
+
+    return $frequency;
+}
+
+function pickingNumbers(array $a): int
+{
+    $frequency = frequency($a, 100);
+    $frequencyLength = count($frequency);
+    $maxLengthSubarray = 0;
+
+    for ($j = 1; $j < $frequencyLength; $j++) {
+        $maxLengthSubarray = max($maxLengthSubarray, $frequency[$j] + $frequency[$j - 1]);
+    }
+
+    return $maxLengthSubarray;
+}
