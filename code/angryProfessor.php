8db19e3b2512902ab916c73f42a9c33602f2ce87
AssoDePicche <samuelprado730@gmail.com>
2022-12-16 12:36:04 -0300

    Refactor: Update delay if conditional to a ternary operation

--- code/angryProfessor.php
+++ code/angryProfessor.php
@@ -5,9 +5,7 @@ function angryProfessor(int $threshold, array $arrivalTime): string
     $inTime = 0;
 
     foreach ($arrivalTime as $delay) {
-        if ($delay <= 0) {
-            $inTime++;
-        }
+        $delay <= 0 && $inTime++;
     }
 
     return $inTime < $threshold ? "YES" : "NO";
