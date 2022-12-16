d9f017c9b779e0a978a3bb9661170088d4a904b8
AssoDePicche <samuelprado730@gmail.com>
2022-12-15 23:32:39 -0300

    Add Cut the Sticks

---
+++ code/cutTheSticks.php
@@ -0,0 +1,33 @@
+<?php
+
+function filter(array $array): array
+{
+    return array_filter($array, function ($value) {
+        if ($value > 0) {
+            return $value;
+        }
+    });
+}
+
+function cutTheSticks(array $sticksLength): array
+{
+    $numberOfSticks = count($sticksLength);
+    $cutsPerIteration = [];
+
+    for ($i = 0; $i < $numberOfSticks; $i++) {
+        $sticksLengthGreaterThanZero = filter($sticksLength);
+        $numberOfCuts = 0;
+        $min = min($sticksLengthGreaterThanZero);
+
+        for ($j = 0; $j < $numberOfSticks; $j++) {
+            if ($sticksLength[$j] > 0) {
+                $sticksLength[$j] -= $min;
+                $numberOfCuts++;
+            }
+        }
+
+        $numberOfCuts > 0 && array_push($cutsPerIteration, $numberOfCuts);
+    }
+
+    return $cutsPerIteration;
+}
