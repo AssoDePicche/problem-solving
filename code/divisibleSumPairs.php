2d613df1e3e073a72e5d342a0f741177735c167f
AssoDePicche <samuelprado730@gmail.com>
2022-12-16 22:56:00 -0300

    Refactor: Update the for loop and changed the if statement to a ternary operation

--- code/divisibleSumPairs.php
+++ code/divisibleSumPairs.php
@@ -1,14 +1,12 @@
 <?php
 
-function divisibleSumPairs(int $n, int $k, array $array): int
+function divisibleSumPairs(int $n, int $k, array $a): int
 {
     $pairs = 0;
 
-    for ($i = 0; $i < count($array); $i++) {
-        for ($j = $i + 1; $j < count($array); $j++) {
-            if ($i < $j && ($array[$i] + $array[$j]) % $k === 0) {
-                $pairs++;
-            }
+    for ($i = 0; $i < $n; $i++) {
+        for ($j = $i + 1; $j < $n; $j++) {
+            $i < $j && ($a[$i] + $a[$j]) % $k === 0 && $pairs++;
         }
     }
 
