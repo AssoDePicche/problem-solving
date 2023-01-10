af66489c2856dcabbba774b4e8736503ee88f069
AssoDePicche <samuelprado730@gmail.com>
2023-01-10 14:19:39 -0300

    Refactor: Change the for loop to a regex function

--- code/Strings/strongPassword.php
+++ code/Strings/strongPassword.php
@@ -1,25 +1,16 @@
 <?php
 
-function charsToMatchPattern(array $needles, string $string): int
-{
-    $chars = 0;
-
-    foreach ($needles as $needle) {
-        $result = strpbrk($needle, $string);
-
-        !is_string($result) && $chars++;
-    }
-
-    return $chars;
-}
-
 function minimumNumber(int $n, string $password): int
 {
-    $needles = [ 'abcdefghijklmnopqrstuvwxyz', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', '!@#$%^&*()-+', '0123456789' ];
+    $minimum = 0;
 
-    $minimum = charsToMatchPattern($needles, $password);
+    !preg_match('/[a-z]/', $password) && $minimum++;
 
-    ($minimum + $n) < 6 && $minimum += 6 - ($minimum + $n);
+    !preg_match('/[A-Z]/', $password) && $minimum++;
 
-    return $minimum;
+    !preg_match('/[0-9]/', $password) && $minimum++;
+
+    !preg_match('/[!@#$%^&*()\-+]/', $password) && $minimum++;
+
+    return ($minimum + $n) < 6 ? $minimum + 6 - ($minimum + $n) : $minimum;
 }
