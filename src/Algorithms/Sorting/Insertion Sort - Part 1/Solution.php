b53623dec347ba5cafaae710a7588bff8b760731
AssoDePicche <samuelprado730@gmail.com>
2023-02-22 16:11:44 -0300

    Refactor: Improve Insertion Sort design

--- src/Algorithms/Sorting/Insertion Sort - Part 1/Solution.php
+++ src/Algorithms/Sorting/Insertion Sort - Part 1/Solution.php
@@ -1,43 +1,31 @@
 <?php
 
-function printArray(iterable $list): void
+function printArray(array $array, int $size): void
 {
-    foreach ($list as $item) {
-        echo $item . ' ';
+    for ($i = 0; $i < $size; $i++) {
+        echo $array[$i] . ' ';
     }
 
     echo PHP_EOL;
 }
 
-function insertionSort1(int $n, array $a): void
+function insertionSort(array $list, int $size)
 {
-    $x = $a[$n - 1];
+    for ($i = 1; $i < $size; $i++) {
+        $temp = $list[$i];
 
-    for (--$n; $n >= 0; $n--) {
-        if ($a[$n] === $x) {
-            continue;
+        $j = $i - 1;
+
+        while ($j >= 0 && $list[$j] > $temp) {
+            $list[$j + 1] = $list[$j];
+            $j = $j - 1;
+            printArray($list, $size);
         }
 
-        if ($a[$n] > $x) {
-            $a[$n + 1] = $a[$n];
-        }
-
-        if ($a[$n] < $x) {
-            $a[$n + 1] = $x;
-        }
-
-        printArray($a);
-
-        if ($a[$n + 1] > $a[$n - 1]) {
-            break;
-        }
+        $list[$j + 1] = $temp;
     }
 
-    if ($x === 1) {
-        $a[0] = $x;
-
-        printArray($a);
-    }
+    printArray($list, $size);
 }
 
 $n = intval(trim(fgets(STDIN)));
@@ -46,4 +34,4 @@ $temp = rtrim(fgets(STDIN));
 
 $array = array_map('intval', preg_split('/ /', $temp, -1, PREG_SPLIT_NO_EMPTY));
 
-insertionSort1($n, $array);
+insertionSort($array, $n);
