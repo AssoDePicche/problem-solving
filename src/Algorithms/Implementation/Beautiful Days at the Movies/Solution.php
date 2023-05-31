ccb1bfae6d98bcd7fe5a093ba75a299d3dbe2cea
AssoDePicche <samuelprado730@gmail.com>
2023-05-31 18:27:08 -0300

    refactor: improve readability

--- src/Algorithms/Implementation/Beautiful Days at the Movies/Solution.php
+++ src/Algorithms/Implementation/Beautiful Days at the Movies/Solution.php
@@ -5,34 +5,24 @@ function reverse(int $integer): int
     return (int) strrev($integer);
 }
 
-function isABeautifulNumber(int $i, int $k): bool
+function isABeautifulDay(int $dayNumber, int $divisor): bool
 {
-    return abs($i - reverse($i)) % $k === 0;
+    return abs($dayNumber - reverse($dayNumber)) % $divisor === 0;
 }
 
-function beautifulDays(int $i, int $j, int $k): int
-{
-    $count = 0;
+function beautifulDays(
+    int $startingDayNumber,
+    int $endingDayNumber,
+    int $divisor
+): int {
+    $beautifulDaysInRange = 0;
 
-    for ($i; $i <= $j; $i++) {
-        isABeautifulNumber($i, $k) && $count++;
+    for ($startingDayNumber; $startingDayNumber <= $endingDayNumber; $startingDayNumber++) {
+        isABeautifulDay(
+            $startingDayNumber,
+            $divisor
+        ) && $beautifulDaysInRange++;
     }
 
-    return $count;
+    return $beautifulDaysInRange;
 }
-
-$fptr = fopen(getenv('OUTPUT_PATH'), 'w');
-
-$first_multiple_input = explode(' ', rtrim(fgets(STDIN)));
-
-$i = intval($first_multiple_input[0]);
-
-$j = intval($first_multiple_input[1]);
-
-$k = intval($first_multiple_input[2]);
-
-$result = beautifulDays($i, $j, $k);
-
-fwrite($fptr, $result . PHP_EOL);
-
-fclose($fptr);
