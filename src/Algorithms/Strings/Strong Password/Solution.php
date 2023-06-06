e761c040e8cdf637102d943120d141d929cdb9d7
AssoDePicche <samuelprado730@gmail.com>
2023-06-06 18:16:52 -0300

    refactor: improve code design of strong password

--- src/Algorithms/Strings/Strong Password/Solution.php
+++ src/Algorithms/Strings/Strong Password/Solution.php
@@ -1,28 +1,20 @@
 <?php
 
-function minimumNumber(int $n, string $password): int
+define('MIN_PASSWORD_LENGTH', 6);
+
+function minimumNumber(int $length, string $password): int
 {
-    $minimum = 0;
+    $charsToAdd = 0;
 
-    !preg_match('/[a-z]/', $password) && $minimum++;
+    !preg_match('/[a-z]/', $password) && $charsToAdd++;
 
-    !preg_match('/[A-Z]/', $password) && $minimum++;
+    !preg_match('/[A-Z]/', $password) && $charsToAdd++;
 
-    !preg_match('/[0-9]/', $password) && $minimum++;
+    !preg_match('/[0-9]/', $password) && $charsToAdd++;
 
-    !preg_match('/[!@#$%^&*()\-+]/', $password) && $minimum++;
+    !preg_match('/[!@#$%^&*()\-+]/', $password) && $charsToAdd++;
 
-    return ($minimum + $n) < 6 ? $minimum + 6 - ($minimum + $n) : $minimum;
+    $matchPasswordSize = (($charsToAdd + $length) >= MIN_PASSWORD_LENGTH);
+
+    return $matchPasswordSize ? $charsToAdd : MIN_PASSWORD_LENGTH - $length;
 }
-
-$fptr = fopen(getenv('OUTPUT_PATH'), 'w');
-
-$n = intval(trim(fgets(STDIN)));
-
-$password = rtrim(fgets(STDIN), "\r\n");
-
-$answer = minimumNumber($n, $password);
-
-fwrite($fptr, $answer . PHP_EOL);
-
-fclose($fptr);
