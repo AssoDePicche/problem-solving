0f955906eaa3ff9c756a62b04882d47d21698cf4
AssoDePicche <samuelprado730@gmail.com>
2023-06-06 14:51:18 -0300

    refactor: improve code design of grading students

--- src/Algorithms/Implementation/Grading Students/Solution.php
+++ src/Algorithms/Implementation/Grading Students/Solution.php
@@ -1,37 +1,24 @@
 <?php
 
+define('MIN_GRADE_FOR_ROUNDING', 38);
+
+function roundGrade(int $grade): int
+{
+    $minRoudingDifference = ($grade % 5);
+
+    return match ($minRoudingDifference) {
+        3 => ($grade + 2),
+        4 => ++$grade,
+        default => $grade
+    };
+}
+
 function gradingStudents(array $grades): array
 {
-    $length = count($grades);
-
-    for ($i = 0; $i < $length; $i++) {
-        if ($grades[$i] < 38) {
-            continue;
-        }
-
-        $modulo = $grades[$i] % 5;
-
-        $modulo === 3 && $grades[$i] += 2;
-
-        $modulo === 4 && $grades[$i]++;
-    }
-
-    return $grades;
+    return array_map(
+        fn (int $grade) => ($grade < MIN_GRADE_FOR_ROUNDING)
+            ? $grade
+            : roundGrade($grade),
+        $grades
+    );
 }
-
-$fptr = fopen(getenv("OUTPUT_PATH"), "w");
-
-$count = intval(trim(fgets(STDIN)));
-
-$grades = array();
-
-for ($i = 0; $i < $count; $i++) {
-    $item = intval(trim(fgets(STDIN)));
-    $grades[] = $item;
-}
-
-$result = gradingStudents($grades);
-
-fwrite($fptr, implode(PHP_EOL, $result) . PHP_EOL);
-
-fclose($fptr);
