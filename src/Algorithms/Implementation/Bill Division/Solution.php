e66803aa34c831fbac60beda3786affc72575642
AssoDePicche <samuelprado730@gmail.com>
2023-06-06 14:00:31 -0300

    refactor: improve code readability

--- src/Algorithms/Implementation/Bill Division/Solution.php
+++ src/Algorithms/Implementation/Bill Division/Solution.php
@@ -1,24 +1,12 @@
 <?php
 
-function bonAppetit(array $bill, int $k, int $b): void
+function bonAppetit(array $bill, int $dishThatAnnaDidNotEat, int $annaContribution): void
 {
-    unset($bill[$k]);
+    unset($bill[$dishThatAnnaDidNotEat]);
 
-    $cost = array_sum($bill) / 2;
+    $annaOrderCost = array_sum($bill) / 2;
 
-    echo ($b === $cost) ? 'Bon Appetit' : abs($b - $cost);
+    $billIsFairlySplit = ($annaContribution === $annaOrderCost);
+
+    echo $billIsFairlySplit ? 'Bon Appetit' : ($annaContribution - $annaOrderCost);
 }
-
-$first_multiple_input = explode(' ', rtrim(fgets(STDIN)));
-
-$n = intval($first_multiple_input[0]);
-
-$k = intval($first_multiple_input[1]);
-
-$bill_temp = rtrim(fgets(STDIN));
-
-$bill = array_map('intval', preg_split('/ /', $bill_temp, -1, PREG_SPLIT_NO_EMPTY));
-
-$b = intval(trim(fgets(STDIN)));
-
-bonAppetit($bill, $k, $b);
