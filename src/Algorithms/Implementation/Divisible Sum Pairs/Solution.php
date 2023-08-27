e38499ae0877b9329288032549c8880a9420619a
AssoDePicche <samuelprado730@gmail.com>
2023-08-27 02:20:18 -0300

    refactor: divisible sum pairs

--- src/Algorithms/Implementation/Divisible Sum Pairs/Solution.php
+++ src/Algorithms/Implementation/Divisible Sum Pairs/Solution.php
@@ -1,32 +1,16 @@
 <?php
 
-function divisibleSumPairs(int $n, int $k, array $a): int
+function divisibleSumPairs(int $arraySize, int $divisor, array $array): int
 {
-    $pairs = 0;
+    $divisibleSumPairs = 0;
 
-    for ($i = 0; $i < $n; $i++) {
-        for ($j = $i + 1; $j < $n; $j++) {
-            $i < $j && ($a[$i] + $a[$j]) % $k === 0 && $pairs++;
+    for ($i = 0; $i < $arraySize; ++$i) {
+        for ($j = $i + 1; $j < $arraySize; ++$j) {
+            $divisibleSumPair = ($array[$i] + $array[$j]) % $divisor === 0;
+
+            $divisibleSumPair && ++$divisibleSumPairs;
         }
     }
 
-    return $pairs;
+    return $divisibleSumPairs;
 }
-
-$fptr = fopen(getenv('OUTPUT_PATH'), 'w');
-
-$first_multiple_input = explode(' ', rtrim(fgets(STDIN)));
-
-$n = intval($first_multiple_input[0]);
-
-$k = intval($first_multiple_input[1]);
-
-$temp = rtrim(fgets(STDIN));
-
-$array = array_map('intval', preg_split('/ /', $temp, -1, PREG_SPLIT_NO_EMPTY));
-
-$result = divisibleSumPairs($n, $k, $array);
-
-fwrite($fptr, $result . PHP_EOL);
-
-fclose($fptr);
