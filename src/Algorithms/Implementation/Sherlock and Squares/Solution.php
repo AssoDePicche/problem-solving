89ccbd9d9f8ee47030e5b58f83bbedc688242c34
AssoDePicche <samuelprado730@gmail.com>
2023-08-29 02:09:42 -0300

    refactor: sherlock and squares

--- src/Algorithms/Implementation/Sherlock and Squares/Solution.php
+++ src/Algorithms/Implementation/Sherlock and Squares/Solution.php
@@ -1,28 +1,10 @@
 <?php
 
-function squares(int $a, int $b): int
+function countPerfectSquares(int $lowerRangeBoundary, int $upperRangeBoundary): int
 {
-    $sqrtA = sqrt($a);
+    $lowerRoot = (int) ceil(sqrt($lowerRangeBoundary));
 
-    $sqrtB = sqrt($b);
+    $upperRoot = (int) floor(sqrt($upperRangeBoundary));
 
-    return max(0, floor($sqrtB) - ceil($sqrtA) + 1);
+    return max(0, $upperRoot - $lowerRoot + 1);
 }
-
-$fptr = fopen(getenv('OUTPUT_PATH'), 'w');
-
-$q = intval(trim(fgets(STDIN)));
-
-for ($q_itr = 0; $q_itr < $q; $q_itr++) {
-    $first_multiple_input = explode(' ', rtrim(fgets(STDIN)));
-
-    $a = intval($first_multiple_input[0]);
-
-    $b = intval($first_multiple_input[1]);
-
-    $result = squares($a, $b);
-
-    fwrite($fptr, $result . PHP_EOL);
-}
-
-fclose($fptr);
