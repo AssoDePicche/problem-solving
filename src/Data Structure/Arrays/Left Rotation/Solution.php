302f743032d382688dc12c943933f1677c4767b9
AssoDePicche <samuelprado730@gmail.com>
2023-09-01 02:10:12 -0300

    refactor: left rotation

--- src/Data Structure/Arrays/Left Rotation/Solution.php
+++ src/Data Structure/Arrays/Left Rotation/Solution.php
@@ -1,41 +1,19 @@
 <?php
 
+define('INITIAL_INDEX', 0);
+
 function rotateLeft(int $rotations, array $array): array
 {
     $arrayLength = count($array);
 
-    if ($rotations === $arrayLength) {
+    if ($arrayLength === 0 || $rotations === 0) {
         return $array;
     }
 
     $rotations %= $arrayLength;
 
-    while ($rotations !== 0) {
-        $aux = array_shift($array);
-
-        array_push($array, $aux);
-
-        $rotations--;
-    }
-
-    return $array;
+    return array_merge(
+        array_slice($array, $rotations),
+        array_slice($array, INITIAL_INDEX, $rotations)
+    );
 }
-
-
-$fptr = fopen(getenv('OUTPUT_PATH'), 'w');
-
-$first_multiple_input = explode(' ', rtrim(fgets(STDIN)));
-
-$n = intval($first_multiple_input[0]);
-
-$d = intval($first_multiple_input[1]);
-
-$arr_temp = rtrim(fgets(STDIN));
-
-$arr = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
-
-$result = rotateLeft($d, $arr);
-
-fwrite($fptr, implode(' ', $result) . PHP_EOL);
-
-fclose($fptr);
