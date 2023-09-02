99f45e54bbfe129ade76addb2a10b766b1383872
AssoDePicche <samuelprado730@gmail.com>
2023-09-02 16:54:04 -0300

    refactor: array - ds

--- src/Data Structure/Arrays/Array - DS/Solution.php
+++ src/Data Structure/Arrays/Array - DS/Solution.php
@@ -1,36 +1,22 @@
 <?php
 
-function reverseArray(array $a): array
+function reverseArray(array $array): array
 {
     $left = 0;
 
-    $right = count($a) - 1;
+    $right = count($array) - 1;
 
     while ($left < $right) {
-        $temp = $a[$right];
+        $temp = $array[$left];
 
-        $a[$right] = $a[$left];
+        $array[$left] = $array[$right];
 
-        $a[$left] = $temp;
+        $array[$right] = $temp;
 
-        $left++;
+        ++$left;
 
-        $right--;
+        --$right;
     }
 
-    return $a;
+    return $array;
 }
-
-$fptr = fopen(getenv('OUTPUT_PATH'), 'w');
-
-$count = intval(trim(fgets(STDIN)));
-
-$temp = rtrim(fgets(STDIN));
-
-$array = array_map('intval', preg_split('/ /', $temp, -1, PREG_SPLIT_NO_EMPTY));
-
-$result = reverseArray($array);
-
-fwrite($fptr, implode(' ', $result) . PHP_EOL);
-
-fclose($fptr);
