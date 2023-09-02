ac27399e1bbded8b2db38608e3185db06d52e370
AssoDePicche <samuelprado730@gmail.com>
2023-09-02 16:55:29 -0300

    refactor: electronics shop

--- src/Algorithms/Implementation/Electronics Shop/Solution.php
+++ src/Algorithms/Implementation/Electronics Shop/Solution.php
@@ -1,46 +1,18 @@
 <?php
 
-function getMoneySpent(array $keyboards, array $drives, int $budget): int
+function maxMoneySpent(array $keyboards, array $drives, int $budget): int
 {
-    $bills = [];
+    $maxMoneySpent = -1;
 
-    foreach ($keyboards as $k) {
+    foreach ($keyboards as $keyboard) {
         foreach ($drives as $drive) {
-            $bills[] = ($k + $drive) <= $budget ? $k + $drive : 0;
+            $totalCost = ($keyboard + $drive);
+
+            if ($totalCost <= $budget) {
+                $maxMoneySpent = max($maxMoneySpent, $totalCost);
+            }
         }
     }
 
-    $max = max($bills);
-
-    return $max !== 0 ? $max : -1;
+    return $maxMoneySpent;
 }
-
-$fptr = fopen(getenv('OUTPUT_PATH'), 'w');
-
-$stdin = fopen('php://stdin', 'r');
-
-fscanf($stdin, "%[^\n]", $bnm_temp);
-
-$bnm = explode(' ', $bnm_temp);
-
-$b = intval($bnm[0]);
-
-$n = intval($bnm[1]);
-
-$m = intval($bnm[2]);
-
-fscanf($stdin, "%[^\n]", $keyboards_temp);
-
-$keyboards = array_map('intval', preg_split('/ /', $keyboards_temp, -1, PREG_SPLIT_NO_EMPTY));
-
-fscanf($stdin, "%[^\n]", $drives_temp);
-
-$drives = array_map('intval', preg_split('/ /', $drives_temp, -1, PREG_SPLIT_NO_EMPTY));
-
-$moneySpent = getMoneySpent($keyboards, $drives, $b);
-
-fwrite($fptr, $moneySpent . PHP_EOL);
-
-fclose($stdin);
-
-fclose($fptr);
