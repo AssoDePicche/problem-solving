a1a1ebaa5bc8008fb3ceac5e859303829c81a65c
AssoDePicche <samuelprado730@gmail.com>
2023-09-03 22:02:10 -0300

    refactor: compare the triplets

--- src/Algorithms/Warmup/Compare the Triplets/Solution.php
+++ src/Algorithms/Warmup/Compare the Triplets/Solution.php
@@ -1,30 +1,18 @@
 <?php
 
-function compareTriplets(array $a, array $b): array
+define('NUMBER_OF_CATEGORIES', 3);
+
+function compareTriplets(array $aliceScores, array $bobScores): array
 {
-    $alice = $bob = 0;
+    $aliceTotalScore = 0;
 
-    for ($i = 0; $i < 3; $i++) {
-        $a[$i] > $b[$i] && $alice++;
+    $bobTotalScore = 0;
 
-        $a[$i] < $b[$i] && $bob++;
+    for ($index = 0; $index < NUMBER_OF_CATEGORIES; ++$index) {
+        $aliceScores[$index] > $bobScores[$index] && $aliceTotalScore++;
+
+        $aliceScores[$index] < $bobScores[$index] && $bobTotalScore++;
     }
 
-    return [$alice, $bob];
+    return [$aliceTotalScore, $bobTotalScore];
 }
-
-$fptr = fopen(getenv('OUTPUT_PATH'), 'w');
-
-$a_temp = rtrim(fgets(STDIN));
-
-$a = array_map('intval', preg_split('/ /', $a_temp, -1, PREG_SPLIT_NO_EMPTY));
-
-$b_temp = rtrim(fgets(STDIN));
-
-$b = array_map('intval', preg_split('/ /', $b_temp, -1, PREG_SPLIT_NO_EMPTY));
-
-$result = compareTriplets($a, $b);
-
-fwrite($fptr, implode(' ', $result) . PHP_EOL);
-
-fclose($fptr);
