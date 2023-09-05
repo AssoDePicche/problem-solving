bafaf2f2ca64d7fa258583bf0120ac1a6d43896c
AssoDePicche <samuelprado730@gmail.com>
2023-09-05 11:37:20 -0300

    perf: number line jumps

--- src/Algorithms/Implementation/Number Line Jumps/Solution.php
+++ src/Algorithms/Implementation/Number Line Jumps/Solution.php
@@ -2,33 +2,19 @@
 
 function kangaroo(int $x1, int $v1, int $x2, int $v2): string
 {
-    for ($i = 0; $i < 10000; $i++) {
-        $x1 += $v1;
-
-        $x2 += $v2;
-
-        if ($x1 === $x2) {
-            return "YES";
-        }
+    if ($v1 === $v2 && $x1 === $x2) {
+        return "YES";
     }
 
-    return "NO";
+    if ($v1 === $v2) {
+        return "NO";
+    }
+
+    $numberOfJumpsNeeded = ($x2 - $x1) / ($v1 - $v2);
+
+    $isAnIntegerNumberOfJumps = floor($numberOfJumpsNeeded) === floatval($numberOfJumpsNeeded);
+
+    $kangaroosMeet = $numberOfJumpsNeeded >= 0 && $isAnIntegerNumberOfJumps;
+
+    return $kangaroosMeet ? "YES" : "NO";
 }
-
-$fptr = fopen(getenv('OUTPUT_PATH'), 'w');
-
-$first_multiple_input = explode(' ', rtrim(fgets(STDIN)));
-
-$x1 = intval($first_multiple_input[0]);
-
-$v1 = intval($first_multiple_input[1]);
-
-$x2 = intval($first_multiple_input[2]);
-
-$v2 = intval($first_multiple_input[3]);
-
-$result = kangaroo($x1, $v1, $x2, $v2);
-
-fwrite($fptr, $result . PHP_EOL);
-
-fclose($fptr);
