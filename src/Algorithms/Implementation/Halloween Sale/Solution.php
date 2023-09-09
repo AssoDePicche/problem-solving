7712a9bf8b23915bd27bf577866bc5677ecb5596
AssoDePicche <samuelprado730@gmail.com>
2023-09-08 22:31:53 -0300

    refactor: halloween sale

--- src/Algorithms/Implementation/Halloween Sale/Solution.php
+++ src/Algorithms/Implementation/Halloween Sale/Solution.php
@@ -1,40 +1,30 @@
 <?php
 
-function howManyGames(int $standardPrice, int $discount, int $minimumPrice, int $budget): int
-{
-    if ($discount === $standardPrice) {
-        return $budget;
+function howManyGames(
+    int $priceOfTheFirstGame,
+    int $discountFromThePreviousGamePrice,
+    int $minimumCostOfAGame,
+    int $startingBudget
+): int {
+    if ($discountFromThePreviousGamePrice === $priceOfTheFirstGame) {
+        return $startingBudget;
     }
 
-    $games = $cart = 0;
+    $gamesToBuy = 0;
 
-    $buy = $standardPrice;
+    $moneySpent = 0;
 
-    while ($cart <= $budget) {
-        $cart += $buy >= $minimumPrice ? $buy : $minimumPrice;
+    $gamePrice = $priceOfTheFirstGame;
 
-        $buy -= $discount;
+    while ($moneySpent <= $startingBudget) {
+        $hasDiscount = ($gamePrice >= $minimumCostOfAGame);
 
-        $cart <= $budget && $games++;
+        $moneySpent += $hasDiscount ? $gamePrice : $minimumCostOfAGame;
+
+        $gamePrice -= $discountFromThePreviousGamePrice;
+
+        $moneySpent <= $startingBudget && ++$gamesToBuy;
     }
 
-    return $games;
+    return $gamesToBuy;
 }
-
-$fptr = fopen(getenv('OUTPUT_PATH'), 'w');
-
-$first_multiple_input = explode(' ', rtrim(fgets(STDIN)));
-
-$p = intval($first_multiple_input[0]);
-
-$d = intval($first_multiple_input[1]);
-
-$m = intval($first_multiple_input[2]);
-
-$s = intval($first_multiple_input[3]);
-
-$answer = howManyGames($p, $d, $m, $s);
-
-fwrite($fptr, $answer . PHP_EOL);
-
-fclose($fptr);
